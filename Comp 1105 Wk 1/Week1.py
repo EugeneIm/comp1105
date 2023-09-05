253d182908020392360d1a1a586e49cf37cbe93d
Eugene <immer277@gmail.com>
2023-09-05 13:16:00 -0700

    Create Week1.py
    Review questions, what I did so far

---
+++ Comp 1105 Wk 1/Week1.py
@@ -0,0 +1,46 @@
+#1 byte is 8 bits
+#1 kb(kilobyte) is 1024 bytes
+#1 MB is 1,048,576 bytes
+#1 GB is 1,073,741,824 bytes
+
+"Question 1"
+#Convert the following numbers to decimals and show your work. 
+#1. 1101(2)
+#2. 10101010(2)
+"Question 2"
+#Conver the decimals numbers 457 and 988 to the binary. You are supposed to use the algorithm for converting decimal to binary.
+
+
+"Question 3"
+#Consider that we only have 1 byte to remember data (1 byte is 8 bits).
+#How many numbers can be represented?
+#How many numbers can be represented if we have N bytes rather than 1 byte?
+
+"Question 4"
+#Use the ASCII encoding table to represent the following characters or strings in binary.
+#1. 124
+""
+#2. %
+#3. D
+#4. a
+#5. Dad
+
+"Question 5"
+#Write a python program that creates a car in the terminal.
+
+print("   ___\n _|   |_ \n|       |\n|_______|\n  O   O")
+
+"Question 6"
+
+print(3, 2+5, 2>5, "Test!", "!", sep=' ', end='>>')
+#"sep='@'" separates all the printed numbers/strings in place of a space like a spacebar gives you
+#as long as the "2+5" and "2>5" are separated by commas, the interpreter will consider them to be statments and give you the answers whether it is false, like the "2>5", and "7" from the "2+5". 
+#   3@7@False@Test!@!>> is the result of the print statement above. 
+
+#if I were to do sep=" ", the print would show as
+#   3 7 False Test! !>>
+
+#take away the "end='>>'"
+#   3@7@False@Test!@!
+
+#so on and so forth, what you take away is taken away from the end product, if I take away the "3" at the very beginning, the product would start with "7" instead of "3". 
