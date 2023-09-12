e4cc4c3b52a93d86229fb92e3d2eeb8b773bf476
Im <eim1@student.douglascollege.ca>
2023-09-12 14:55:55 -0700

    True or false

---
+++ Comp 1105 Wk 2/Wk2ReviewTF.py
@@ -0,0 +1,46 @@
+'''
+1. Syntax (a)
+Syntax
+
+2. Task (a)
+
+3. Algorithm (d)
+
+4. Pseudocode (b)
+
+5. Flow Chart (a)
+
+6. String (c)
+
+7. Variable (a)
+
+8. User (b)
+
+9. Either single or double quotations (d)
+
+10. Comments (a)
+
+11. Variable Declaration (a)
+
+12. # (d)
+
+13. 17 = x will give an error (b)
+
+14. Math Expressions (d)
+
+15. / (d)
+
+16. ** (c)
+
+17. % (a)
+
+18. Float (b)
+
+19. input() (a)
+
+20. float() (b)
+
+21. An unexplained number in the code (b)
+
+22. Named constant (b)
+'''
