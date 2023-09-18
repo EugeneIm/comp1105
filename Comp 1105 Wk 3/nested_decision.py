f23ad47c87300bb55236cf0a275c5717d5b2ce67
Eugene <immer277@gmail.com>
2023-09-18 13:32:38 -0700

    updated nested if statements

--- Comp 1105 Wk 3/nested_decision.py
+++ Comp 1105 Wk 3/nested_decision.py
@@ -6,11 +6,11 @@
 def main():
 	salary = int(input("What is your current salary? "))
 	years_on_the_job = int(input("How long have you worked here? "))
-	if salary < 30000:
+	if (salary < 30000) and (years_on_the_job > 2):
 		print("You must have an annual salary of 30,000 or more")
-	if years_on_the_job < 2:
+	if (salary > 30000) and (years_on_the_job < 2):
 		print("You must have had to work here for more 2 years")
-	if salary >= 30000 and years_on_the_job > 2:
+	if salary > 30000 and years_on_the_job > 2:
 		print("You qualify for the loan")
 main()
 
