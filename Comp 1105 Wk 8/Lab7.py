72db0c190b0e9f3fcb7eb162b1483a79dc9bc85c
Im <eim1@student.douglascollege.ca>
2023-10-26 15:45:43 -0700

    accessing files with python

--- Comp 1105 Wk 8/Lab7.py
+++ Comp 1105 Wk 8/Lab7.py
@@ -3,7 +3,6 @@
 
 # ZeroDivisionError
 # FileNotFoundError
-#
 try:
     print(5/0)
 except ZeroDivisionError:
@@ -25,5 +24,68 @@ while True:
     else:
         print(answer)
 
-file = "two.txt"
+#Reading the file
+try:
+    file_read = open("data.txt", "r")
+    #r is read, w is write, a is append
+    data = file_read.read()
+    #.read(), .write(), .append()
+    print(data)
 
+    file_read.close()
+    #close the file
+except FileNotFoundError:
+    print("File is not found")
+
+#While loop to read a file
+try:
+    file_content = open('data.txt', 'r')
+    while True:
+        line = file_content.readline()
+        if line == '':
+            break
+    file_content.close()
+except FileNotFoundError:
+    print("File not found")
+
+#Writing to a file
+try:
+    content = open('out.txt', 'w')
+    file_content = ("This writes to a file")
+    info = content.write(file_content)
+    print(info)
+    content.close()
+except FileNotFoundError:
+    print("file not found")
+
+
+#Calculate the average while reading the file
+'''
+1. Read the contents of the file
+2. Store the numbers into a variable
+3. Count how many values were read
+4. Divide the variable by the amount of lines
+
+'''
+
+def main():
+    try:
+        numbers = open('data.txt', 'r')
+        total = 0
+        count = 0
+        while True:
+            line = numbers.readline()
+            if line == '':
+                break
+            count += 1
+            num = float(line)
+            total += num
+        print("The average in data.txt is:", total/count)
+        numbers.close()
+    except FileNotFoundError:
+        print("file not found")
+        return
+    except ValueError as verr:
+        print("MyError : ", verr)
+
+main()
