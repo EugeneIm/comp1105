a2c5229d100828e9baf50041fe335a83468ebdc9
Eugene <immer277@gmail.com>
2023-10-26 14:02:58 -0700

    update lab7

--- Comp 1105 Wk 8/Lab7.py
+++ Comp 1105 Wk 8/Lab7.py
@@ -1,1 +1,7 @@
 #Write a modular program that receives data from a file called 'data.txt' and prints out the contents 
+#Try except functions
+
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("cannot divide by 0")
