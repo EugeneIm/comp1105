32dacacf590b0c0d9c0cfb604a22235d87eea8d7
Im <eim1@student.douglascollege.ca>
2023-11-09 15:47:21 -0800

    lab 8 Q3 started

--- Comp 1105 Wk 8/lab8.py
+++ Comp 1105 Wk 8/lab8.py
@@ -16,15 +16,38 @@ print(full_dict)
 #Q2 function called getStat(), given a file name, returns a dictionary of the words and numbers and their occurence in the file. 
     #Function is case sensitive (use either .lower(), .upper(), .title())
 
-def getStat(fileOne):
+def getStat():
     file = open("one.txt", 'r')
     content = file.read()
-    file.close("one.txt")
-    dict = {}
+    file.close()
+    dictionary = {}
     data = content.split()
     for data in data:
         data = data
         if data != None:
-            dict[data] = dict.get(data, 0) + 1
-    return data
- 
+            dictionary[data] = dictionary.get(data, 0) + 1
+    return dictionary
+full_dictionary = getStat()
+print(full_dictionary)
+
+
+#Q3 use the function from Q2 and read the contents of 2 files and compare them to do the following:
+    #create a function named uniqueWords() where it returns a list of all words that were listed only once
+    #function called moreThanOnce() where it returns a list of words listed more than once
+    #function called onlyInFirst(), returns the words in the first list and not the second
+    #function called moreThanTwice(), returns words that were listed more than twice 
+    #main function to call all the above functions 
+
+def getStat():
+    file = open("one.txt", 'r')
+    content = file.read()
+    file.close()
+    dictionary = {}
+    data = content.split()
+    for data in data:
+        data = data
+        if data != None:
+            dictionary[data] = dictionary.get(data, 0) + 1
+    return dictionary
+full_dictionary = getStat()
+print(full_dictionary)
