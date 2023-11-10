ebb7de6c198635b54f1221d22d4ba2925401552f
Im <eim1@student.douglascollege.ca>
2023-11-09 16:14:12 -0800

    wk9 lab test programming answer

--- Comp 1105 Wk 9/Dictionary.py
+++ Comp 1105 Wk 9/Dictionary.py
@@ -1,6 +1,34 @@
-#Dictionaries are key value pairs
+import random
+#Lab Quiz 9 programming answer
+def getData():
+    n = int(input("Give me a number between 0 and 100: "))
+    while n < 0:
+        n = int(input("Number cannot be negative"))
+    return n
+def randNum():
+    return random.randint(1, 1000)
 
-contact_numbers = {"John":6044010942, "Bokhee":6044010458, "Andrew":7789542358, "Me":7788597089}
+def getKey():
+    
+    choice = random.randint(0,1)
+    if choice == 0:
+        key = chr(random.randint(ord('a'), ord('z')))
+    else:
+        key = chr(random.randint(ord('A'), ord('Z')))
 
-print(contact_numbers)
+    return key
 
+def ranItem():
+    return(getKey(), randNum())
+
+def main():
+    num = getData()
+    d = {}
+    for n in range(num):
+        k, v = ranItem()
+        while k in d:
+            k, v = ranItem()
+        d[k] = v
+    print(d)
+
+main()
